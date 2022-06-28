ef2d273f3d5edb7839eb8d9c81b20bb2b66fc00c
OvercomingJunk <overcomingjunk@yandex.ru>
2022-06-28 15:37:37 +0300

    Slf4j - вывод exception. [#268853]

--- src/main/java/ru/job4j/io/EchoServer.java
+++ src/main/java/ru/job4j/io/EchoServer.java
@@ -1,11 +1,16 @@
 package ru.job4j.io;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
 
 /**
  * Класс демонстрирует работу сервера под видом бота
+ * В классе используется логгирование {@link org.slf4j.Logger},
+ * для вывода информации, если будет выброшен {@link IOException}
  * Клиент отправляет запросы серверу, а сервер на них отвечает
  * В качестве клиента используется программа cURL
  * Пример запроса: curl -i "http://localhost:9000/?msg=Hello"
@@ -14,10 +19,15 @@ import java.net.Socket;
  * На любое другое сообщение ответ будет "What"
  *
  * @author ARTEM CHERNIKOV
- * @version 1.0
+ * @version 1.1
  */
 public class EchoServer {
-    public static void main(String[] args) throws IOException {
+    /**
+     * Поле лог
+     */
+    private static final Logger LOG = LoggerFactory.getLogger(UsageLog4j.class.getName());
+
+    public static void main(String[] args) {
         try (ServerSocket server = new ServerSocket(9000)) {
             while (!server.isClosed()) {
                 Socket socket = server.accept();
@@ -38,6 +48,8 @@ public class EchoServer {
                     out.flush();
                 }
             }
+        } catch (IOException e) {
+            LOG.error("Exception in log example", e);
         }
     }
 }
