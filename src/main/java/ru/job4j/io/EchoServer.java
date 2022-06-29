27b8ff218b8ae2477efaaf95b1aa08dc1c971d15
OvercomingJunk <overcomingjunk@yandex.ru>
2022-06-29 15:30:37 +0300

    Подкорректировал класс EchoServer

--- src/main/java/ru/job4j/io/EchoServer.java
+++ src/main/java/ru/job4j/io/EchoServer.java
@@ -39,9 +39,14 @@ public class EchoServer {
                         System.out.println(str);
                         if (str.contains("msg=")) {
                             switch (str) {
-                                case "GET " + "/?msg=Hello" + " HTTP/1.1" -> out.write("Hello\r\n\r\n".getBytes());
-                                case "GET " + "/?msg=Exit" + " HTTP/1.1" -> server.close();
-                                default -> out.write("What\r\n\r\n".getBytes());
+                                case "GET /?msg=Hello HTTP/1.1":
+                                    out.write("Hello\r\n\r\n".getBytes());
+                                    break;
+                                case "GET /?msg=Exit HTTP/1.1":
+                                    server.close();
+                                    break;
+                                default:
+                                    out.write("What\r\n\r\n".getBytes());
                             }
                         }
                     }
