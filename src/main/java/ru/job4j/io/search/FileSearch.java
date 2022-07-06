5f5c6391f5351a39620e1b72a778ca5107daac94
OvercomingJunk <overcomingjunk@yandex.ru>
2022-07-06 17:09:23 +0300

    Поиск файлов по критерию [#783]

--- src/main/java/ru/job4j/io/search/FileSearch.java
+++ src/main/java/ru/job4j/io/search/FileSearch.java
@@ -1,6 +1,7 @@
 package ru.job4j.io.search;
 
 import ru.job4j.io.ArgsName;
+import ru.job4j.io.SearchFiles;
 
 import static ru.job4j.io.Search.*;
 
@@ -10,7 +11,25 @@ import java.nio.file.Paths;
 import java.util.List;
 import java.util.function.Predicate;
 
+/**
+ * Класс описывает модель программы, для поиска файла по имени, маске или регулярному выражению
+ * Используется класс {@link SearchFiles} - для поиска и фильтрации файлов и директорий
+ * и {@link ArgsName} - для валидции входных параметров и получения значения по ключу
+ *
+ * @author ARTEM CHERNIKOV
+ * @version 1.0
+ */
 public class FileSearch {
+    /**
+     * Метод используется для определения по какому критерию искать файл
+     * 1) Если в тип поиска "name" - то сравниваем файлы для дальнейшей фильтрации по имени файла
+     * 2) Если в тип поиска "regex" - то сравниваем файлы для дальнейшей фильтрации по регулярному выражению
+     * 2) Если в тип поиска "mask" - то переводим маску в регулярное выражение и
+     * сравниваем файлы для дальнейшей фильтрации по регулярному выражению
+     *
+     * @param args - {@link ArgsName} - для удобства изъятия входных парметров запуска
+     * @return - возвращает {@link Predicate<Path>} для фильтрации ненужных файлов
+     */
     private static Predicate<Path> getSearchCondition(ArgsName args) {
         Predicate<Path> predicate;
         String searchType = args.get("t");
@@ -32,6 +51,12 @@ public class FileSearch {
         return predicate;
     }
 
+    /**
+     * Метод используется для валидации вхожных параметров запуска
+     *
+     * @param args - входные параметры запуска
+     * @return - возвращает {@link ArgsName}, для удобства изъятия входных парметров запуска
+     */
     public ArgsName validate(String[] args) {
         ArgsName argsName = ArgsName.of(args);
         if (args.length != 4) {
@@ -55,7 +80,14 @@ public class FileSearch {
         return argsName;
     }
 
-
+    /**
+     * Метод используется для записи абсолютных путей (совпадений имен файлов с искомым файлом) в отдельный файл
+     * Для поиска используем метод {@link ru.job4j.io.Search#search(Path, Predicate)} где
+     * path - это директория в которой начинаем поиск, а Predicate - способ фильтрации, полученный
+     * с помощью метода {@link FileSearch#getSearchCondition(ArgsName)}
+     *
+     * @param argsName - {@link ArgsName}
+     */
     public void writeFiles(ArgsName argsName) {
         Path start = Paths.get(argsName.get("d"));
         try {
--- src/main/java/ru/job4j/io/search/StartSearch.java
+++ src/main/java/ru/job4j/io/search/StartSearch.java
@@ -2,6 +2,19 @@ package ru.job4j.io.search;
 
 import ru.job4j.io.ArgsName;
 
+/**
+ * Класс используется для точки запуска программы программы для поиска файлов
+ * Необходимо запуска с входными параметрами запуска, например:
+ * "-d=c:/ -n=*.?xt -t=mask -o=log.txt"
+ * где d - директория в которой нужно начинать поиск,
+ * n - имя файла, маска или регулярное выражение (искомого файла или файлов)
+ * t - тип поиска (mask искать по маске, name по полному совпадению имени, regex по регулярному выражению)
+ * o - для записи найденных путей к файлам в отдельный файл
+ * Используется класс {@link FileSearch}
+ *
+ * @author ARTEM CHERNIKOV
+ * @version 1.0
+ */
 public class StartSearch {
     public static void main(String[] args) {
         FileSearch search = new FileSearch();
