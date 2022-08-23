347d2b91553b393cdb9a818c6fc116756e303345
OvercomingJunk <overcomingjunk@yandex.ru>
2022-08-23 22:11:36 +0300

    Шаблонизатор. [#855]

--- src/test/java/ru/job4j/template/TemplateAppTest.java
+++ src/test/java/ru/job4j/template/TemplateAppTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.assertj.core.api.Assertions.*;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -44,4 +45,16 @@ class TemplateAppTest {
         map.put("subject", null);
         assertThrows(IllegalArgumentException.class, () -> templateApp.produce(template, map));
     }
+
+    @Test
+    void whenProduce() {
+        TemplateApp templateApp = new TemplateApp();
+        String template = "I am a ${name}, Who are ${subject}? ";
+        Map<String, String> map = new HashMap<>();
+        map.put("name", "Artem");
+        map.put("subject", "you");
+        String expected = templateApp.produce(template, map);
+        assertThat(expected).isEqualTo("I am a Artem, Who are you ");
+    }
+
 }
