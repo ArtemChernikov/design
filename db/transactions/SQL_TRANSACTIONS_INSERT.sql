5f1f960f276bff5a0022b57b0d4fb76cb33eaad5
OvercomingJunk <overcomingjunk@yandex.ru>
2022-09-06 14:49:31 +0300

    Транзакции. Работа с транзакциями в PostgreSQL [#504813]

---
+++ db/transactions/SQL_TRANSACTIONS_INSERT.sql
@@ -0,0 +1,27 @@
+BEGIN
+TRANSACTION ISOLATION LEVEL REPEATABLE READ;
+
+SAVEPOINT empty_table;
+
+INSERT INTO products (name, producer, count, price)
+VALUES ('product_1', 'producer_1', 3, 50),
+       ('product_2', 'producer_2', 13, 100),
+       ('product_3', 'producer_3', 23, 256),
+       ('product_4', 'producer_1', 53, 34),
+       ('product_5', 'producer_3', 103, 11),
+       ('product_6', 'producer_3', 33, 22);
+
+SELECT *
+FROM products*;
+
+ROLLBACK TO empty_table;
+
+INSERT INTO products (name, producer, count, price)
+VALUES ('product_1', 'producer_1', 100, 50),
+       ('product_2', 'producer_2', 13, 100),
+       ('product_3', 'producer_3', 23, 256),
+       ('product_4', 'producer_1', 99, 34),
+       ('product_5', 'producer_3', 103, 11),
+       ('product_6', 'producer_3', 231, 22);
+
+COMMIT;
---
+++ db/transactions/SQL_TRANSACTIONS_TABLE.sql
@@ -0,0 +1,8 @@
+CREATE TABLE products
+(
+    id       serial PRIMARY KEY,
+    name     varchar(50),
+    producer varchar(50),
+    count    integer DEFAULT 0,
+    price    integer
+);
