c6f06cc6e0a94949979c5fe768dad64a7b6ec6df
OvercomingJunk <overcomingjunk@yandex.ru>
2022-10-28 14:37:47 +0300

    Парковка машин [#853 #351288]

--- src/main/java/ru/job4j/ood/lsp/parking/CarParking.java
+++ src/main/java/ru/job4j/ood/lsp/parking/CarParking.java
@@ -6,8 +6,6 @@ import java.util.Set;
 public class CarParking implements Parking {
     private final Set<Car> carParking;
     private final Set<Car> truckParking;
-    private final int carParkingSpaces;
-    private final int truckParkingSpaces;
     private int freeCarParkingSpaces;
     private int freeTruckParkingSpaces;
     private int occupiedCarParkingSpaces = 0;
@@ -15,8 +13,6 @@ public class CarParking implements Parking {
 
 
     public CarParking(int carParkingSpaces, int truckParkingSpaces) {
-        this.carParkingSpaces = carParkingSpaces;
-        this.truckParkingSpaces = truckParkingSpaces;
         freeCarParkingSpaces = carParkingSpaces;
         freeTruckParkingSpaces = truckParkingSpaces;
         carParking = new HashSet<>(carParkingSpaces);
@@ -27,54 +23,24 @@ public class CarParking implements Parking {
     public boolean addCar(Car car) {
         boolean rsl = false;
         int carSize = car.getSize();
-        if (carSize == 1 && freeCarParkingSpaces > 0) {
-            carParking.add(car);
-            freeCarParkingSpaces -= 1;
-            occupiedCarParkingSpaces += 1;
-            rsl = true;
-        } else if (freeTruckParkingSpaces > 0) {
-            truckParking.add(car);
-            freeTruckParkingSpaces -= 1;
-            occupiedTruckParkingSpaces += 1;
-            rsl = true;
-        } else if (freeCarParkingSpaces >= carSize) {
-            carParking.add(car);
-            freeTruckParkingSpaces -= carSize;
-            occupiedCarParkingSpaces += carSize;
-            rsl = true;
+        if (!carParking.contains(car) && !truckParking.contains(car)) {
+            if (carSize == PassengerCar.SIZE && freeCarParkingSpaces > 0) {
+                carParking.add(car);
+                freeCarParkingSpaces -= 1;
+                occupiedCarParkingSpaces += 1;
+                rsl = true;
+            } else if (carSize > PassengerCar.SIZE && freeTruckParkingSpaces > 0) {
+                truckParking.add(car);
+                freeTruckParkingSpaces -= 1;
+                occupiedTruckParkingSpaces += 1;
+                rsl = true;
+            } else if (freeCarParkingSpaces >= carSize) {
+                carParking.add(car);
+                freeCarParkingSpaces -= carSize;
+                occupiedCarParkingSpaces += carSize;
+                rsl = true;
+            }
         }
         return rsl;
-    }
-
-    public int getCarParkingSpaces() {
-        return carParkingSpaces;
-    }
-
-    public int getTruckParkingSpaces() {
-        return truckParkingSpaces;
-    }
-
-    public int getFreeCarParkingSpaces() {
-        return freeCarParkingSpaces;
-    }
-
-    public int getFreeTruckParkingSpaces() {
-        return freeTruckParkingSpaces;
-    }
-
-    public int getOccupiedCarParkingSpaces() {
-        return occupiedCarParkingSpaces;
-    }
-
-    public int getOccupiedTruckParkingSpaces() {
-        return occupiedTruckParkingSpaces;
-    }
-
-    public Set<Car> getCarParking() {
-        return new HashSet<>(carParking);
-    }
-
-    public Set<Car> getTruckParking() {
-        return new HashSet<>(truckParking);
     }
 }
