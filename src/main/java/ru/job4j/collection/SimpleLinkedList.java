bf79bc241462d329fc96bbe394561ee0990d312a
Artem Chernikov <overcomingjunk@yandex.ru>
2024-04-05 12:57:36 +0300

    SimpleLinked

--- src/main/java/ru/job4j/collection/SimpleLinkedList.java
+++ src/main/java/ru/job4j/collection/SimpleLinkedList.java
@@ -5,176 +5,75 @@ import java.util.Iterator;
 import java.util.NoSuchElementException;
 import java.util.Objects;
 
-/**
- * Класс описывает модель односвязного списка со свойствами <b>head</b>, <b>tail</b>, <b>size</b> и <b>modCount</b>
- * Класс реализовывает интерфейс {@link LinkedList}
- * В классе используется вложенный класс {@link Node}
- *
- * @author ARTEM CHERNIKOV
- * @version 1.0
- */
-public class SimpleLinkedList<E> implements LinkedList<E> {
-    /**
-     * Приватный статический вложенный класс для обозначения
-     * узлов в коллекции со свойствами <b>item</b> и <b>next</b>
-     * Каждый узел имеет в себе сам элемент типа {@link E}
-     * и следующий узел типа {@link Node}
-     *
-     * @param <E> - тип элемента
-     */
-    private static class Node<E> {
-        /**
-         * Поле элемента узла
-         */
-        E item;
-        /**
-         * Поле следующего узла типа {@link Node}
-         */
-        Node<E> next;
+public class SimpleLinkedList<E> implements SimpleLinked<E> {
 
-        /**
-         * Конструктор - создание нового узла с определенными значениями
-         *
-         * @param element - элемент
-         * @param next    - следующий узел
-         */
-        Node(E element, Node<E> next) {
-            this.item = element;
-            this.next = next;
-        }
-    }
+    private int size;
 
-    /**
-     * Поле первого узла коллекции
-     */
+    private int modCount;
+
     private Node<E> head;
-    /**
-     * Поле последнего узла коллекции
-     */
-    private Node<E> tail;
-    /**
-     * Поле количества элементов в коллекции
-     */
-    private int size = 0;
-    /**
-     * Поле счетчика изменений в коллекции (добавление элементов)
-     */
-    private int modCount = 0;
 
-    /**
-     * Метод используется для добавления узла в коллекцию
-     * 1) Если коллекция пустая, присваиваем {@link SimpleLinkedList#head}
-     * новый узел с элементом и с ссылкой на следующий узел - null
-     * 2) Если в коллекции только один элемент
-     * 2.1) Присваиваем {@link SimpleLinkedList#tail} новый узел с элементом и ссылкой на следующий узел null
-     * 2.2) Меняем уже существующему первому узлу ссылку на следующий узел {@link SimpleLinkedList#tail}
-     * 3) Если в коллекции больше двух элементов, присваиваем {@link SimpleLinkedList#tail} ссылку на
-     * следующий, новый узел и меняем значения последнего эелемента коллекции на новый узел
-     *
-     * @param value - элемент
-     */
     @Override
     public void add(E value) {
-        if (size == 0) {
-            head = new Node<>(value, null);
-            size++;
-            modCount++;
-        } else if (size == 1) {
-            tail = new Node<>(value, null);
-            head.next = tail;
-            size++;
-            modCount++;
-        } else if (size > 2) {
-            Node<E> newNode = new Node<>(value, null);
-            tail.next = newNode;
-            tail = newNode;
-            size++;
-            modCount++;
+        Node<E> newElement = new Node<>(value, null);
+        if (head == null) {
+            head = newElement;
+        } else {
+            Node<E> current = head;
+            while (current.next != null) {
+                current = current.next;
+            }
+            current.next = newElement;
         }
+        size++;
+        modCount++;
     }
 
-    /**
-     * Метод используется для получения элемента коллекции по индексу
-     * 1) Проверяется не выходит ли index заменяемого элемента за пределы размеров коллекции
-     * с помощью метода {@link Objects#checkIndex(int, int)}
-     * 2) Создаем локальную переменную типа {@link Node}
-     * 3) Если искомый индекс равен нулю, то искомый элемент находится в первом узле
-     * 4) Если искомый индекс равен последнему элементу коллекции, то искомый элемент находится в последнем узле
-     * 5) Иначе запускается цикл, с каждой итерацией переходим по ссылкам на узлы, начиная с первого
-     * и как переменная i станет равна индексу, то это значит, что мы дошли до искомого элемента
-     *
-     * @param index - индекс искомого элемента
-     * @return - возвращает искомый элемент
-     */
     @Override
     public E get(int index) {
         Objects.checkIndex(index, size);
-        Node<E> x;
-        if (index == 0) {
-            x = head;
-        } else if (index == size - 1) {
-            x = tail;
-        } else {
-            x = head;
-            for (int i = 1; i != index; i++) {
-                x = x.next;
-            }
+        Node<E> current = head;
+        int indexElement = 0;
+        while (indexElement < index) {
+            current = current.next;
+            indexElement++;
         }
-        return x.item;
+        return current.item;
     }
 
-    /**
-     * Анонимный класс {@link Iterator} для корректного перебора элементов коллекции
-     *
-     * @return - возвращает итератор
-     */
     @Override
     public Iterator<E> iterator() {
-        return new Iterator<>() {
-            /**
-             * Поле изменений для недопущения изменений в коллекции во время работы итератора
-             */
-            private final int iteratorModCount = modCount;
-            /**
-             * Поле для указателя узла, на котором сейчас находится итератор
-             */
-            private Node<E> iteratorNode = head;
 
-            /**
-             * Метод позволяет узнать, есть ли следующий элемент в коллекции
-             * 1) Проверяем изменилась ли наша коллекция за время работы итератора
-             * если изменилась, то выбрасывается исключение {@link ConcurrentModificationException}
-             * 2) Проверяем равен ли указатель узла null
-             * @return - true - если следующий элемент есть, false - если нет
-             */
+        return new Iterator<E>() {
+            Node<E> current = new Node<>(null, head);
+            final int expectedModCount = modCount;
+
             @Override
             public boolean hasNext() {
-                if (iteratorModCount != modCount) {
+                if (expectedModCount != modCount) {
                     throw new ConcurrentModificationException();
                 }
-                return iteratorNode != null;
+                return current.next != null;
             }
 
-            /**
-             * Метод используется для передвижения указателя итератора по коллекции
-             * и возврата соответствующего значения
-             * 1) Проверяем методом {@link Iterator#hasNext()} есть ли следующий элемент в коллекции
-             * если нет, то выбрасывается {@link NoSuchElementException}
-             * 2) Присваиваем локальной переменной элемент узла коллекции
-             * 3) Переводим указатель на следующий элемент
-             * 4) Возвращаем переменную с элементом
-             *
-             * @return - возвращаем следующий элемент коллекции
-             */
             @Override
             public E next() {
                 if (!hasNext()) {
                     throw new NoSuchElementException();
                 }
-                E rsl = iteratorNode.item;
-                iteratorNode = iteratorNode.next;
-                return rsl;
+                current = current.next;
+                return current.item;
             }
         };
+    }
+
+    private static class Node<E> {
+        private E item;
+        private Node<E> next;
+
+        Node(E element, Node<E> next) {
+            this.item = element;
+            this.next = next;
+        }
     }
 }
--- src/test/java/ru/job4j/collection/SimpleLinkedListTest.java
+++ src/test/java/ru/job4j/collection/SimpleLinkedListTest.java
@@ -1,106 +1,92 @@
 package ru.job4j.collection;
 
-import org.hamcrest.core.Is;
-import org.junit.jupiter.api.Assertions;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-
-import java.util.ConcurrentModificationException;
 import java.util.Iterator;
 
-import static org.hamcrest.MatcherAssert.assertThat;
-import static org.junit.jupiter.api.Assertions.assertThrows;
+class SimpleLinkedListTest {
 
-public class SimpleLinkedListTest {
+    private SimpleLinked<Integer> list;
 
-    @Test
-    public void whenAddAndGet() {
-        LinkedList<Integer> list = new SimpleLinkedList<>();
+    @BeforeEach
+    public void initData() {
+        list = new SimpleLinkedList<>();
         list.add(1);
         list.add(2);
-        assertThat(list.get(0), Is.is(1));
-        assertThat(list.get(1), Is.is(2));
     }
 
     @Test
-    public void whenGetFromOutOfBoundThenExceptionThrown() {
-        LinkedList<Integer> list = new SimpleLinkedList<>();
-        list.add(1);
-        list.add(2);
-        assertThrows(IndexOutOfBoundsException.class, () -> list.get(2));
-    }
-
-    @Test
-    public void whenAddIterHasNextTrue() {
-        LinkedList<Integer> list = new SimpleLinkedList<>();
-        list.add(1);
-        Iterator<Integer> it = list.iterator();
-        assertThat(it.hasNext(), Is.is(true));
-    }
-
-    @Test
-    public void whenAddIterNextOne() {
-        LinkedList<Integer> list = new SimpleLinkedList<>();
-        list.add(1);
-        Iterator<Integer> it = list.iterator();
-        assertThat(it.next(), Is.is(1));
-    }
-
-    @Test
-    public void whenEmptyIterHashNextFalse() {
-        LinkedList<Integer> list = new SimpleLinkedList<>();
-        Iterator<Integer> it = list.iterator();
-        assertThat(it.hasNext(), Is.is(false));
-    }
-
-    @Test
-    public void whenAddIterMultiHasNextTrue() {
-        LinkedList<Integer> list = new SimpleLinkedList<>();
-        list.add(1);
-        Iterator<Integer> it = list.iterator();
-        assertThat(it.hasNext(), Is.is(true));
-        assertThat(it.hasNext(), Is.is(true));
-    }
-
-    @Test
-    public void whenAddIterNextOneNextTwo() {
-        LinkedList<Integer> list = new SimpleLinkedList<>();
-        list.add(1);
-        list.add(2);
-        Iterator<Integer> it = list.iterator();
-        assertThat(it.next(), Is.is(1));
-        assertThat(it.next(), Is.is(2));
-    }
-
-    @Test
-    public void whenGetIteratorTwiceThenEveryFromBegin() {
-        LinkedList<Integer> list = new SimpleLinkedList<>();
-        list.add(1);
-        list.add(2);
-        Iterator<Integer> first = list.iterator();
-        assertThat(first.hasNext(), Is.is(true));
-        assertThat(first.next(), Is.is(1));
-        assertThat(first.hasNext(), Is.is(true));
-        assertThat(first.next(), Is.is(2));
-        assertThat(first.hasNext(), Is.is(false));
-        Iterator<Integer> second = list.iterator();
-        assertThat(second.hasNext(), Is.is(true));
-        assertThat(second.next(), Is.is(1));
-        assertThat(second.hasNext(), Is.is(true));
-        assertThat(second.next(), Is.is(2));
-        assertThat(second.hasNext(), Is.is(false));
-    }
-
-    @Test
-    public void whenAddAfterGetIteratorThenMustBeException() {
-        LinkedList<Integer> list = new SimpleLinkedList<>();
-        Iterator<Integer> iterator = list.iterator();
+    void checkIteratorSimple() {
+        assertThat(list).hasSize(2);
+        list.add(3);
         list.add(4);
-        assertThrows(ConcurrentModificationException.class, iterator::next);
+        assertThat(list).hasSize(4);
     }
 
     @Test
-    public void whenGetIteratorFromEmptyListThenHasNextReturnFalse() {
-        LinkedList<Integer> list = new SimpleLinkedList<>();
-        Assertions.assertFalse(list.iterator().hasNext());
+    void whenAddAndGet() {
+        list.add(3);
+        list.add(4);
+        assertThat(list.get(0)).isEqualTo(1);
+        assertThat(list.get(1)).isEqualTo(2);
+        assertThat(list.get(2)).isEqualTo(3);
+        assertThat(list.get(3)).isEqualTo(4);
+    }
+
+    @Test
+    void whenGetFromOutOfBoundThenExceptionThrown() {
+        assertThatThrownBy(() -> list.get(2))
+                .isInstanceOf(IndexOutOfBoundsException.class);
+    }
+
+    @Test
+    void whenAddIterHasNextTrue() {
+        Iterator<Integer> iterator = list.iterator();
+        assertThat(iterator.hasNext()).isTrue();
+    }
+
+    @Test
+    void whenAddIterNextOne() {
+        Iterator<Integer> iterator = list.iterator();
+        assertThat(iterator.next()).isEqualTo(1);
+    }
+
+    @Test
+    void whenEmptyIterHashNextFalse() {
+        SimpleLinkedList<Integer> list = new SimpleLinkedList<>();
+        Iterator<Integer> iterator = list.iterator();
+        assertThat(iterator.hasNext()).isFalse();
+    }
+
+    @Test
+    void whenAddIterMultiHasNextTrue() {
+        Iterator<Integer> iterator = list.iterator();
+        assertThat(iterator.hasNext()).isTrue();
+        assertThat(iterator.hasNext()).isTrue();
+    }
+
+    @Test
+    void whenAddIterNextOneNextTwo() {
+        Iterator<Integer> iterator = list.iterator();
+        assertThat(iterator.next()).isEqualTo(1);
+        assertThat(iterator.next()).isEqualTo(2);
+    }
+
+    @Test
+    void whenGetIteratorTwiceThenEveryFromBegin() {
+        Iterator<Integer> first = list.iterator();
+        assertThat(first.hasNext()).isTrue();
+        assertThat(first.next()).isEqualTo(1);
+        assertThat(first.hasNext()).isTrue();
+        assertThat(first.next()).isEqualTo(2);
+        assertThat(first.hasNext()).isFalse();
+        Iterator<Integer> second = list.iterator();
+        assertThat(second.hasNext()).isTrue();
+        assertThat(second.next()).isEqualTo(1);
+        assertThat(second.hasNext()).isTrue();
+        assertThat(second.next()).isEqualTo(2);
+        assertThat(second.hasNext()).isFalse();
     }
 }
