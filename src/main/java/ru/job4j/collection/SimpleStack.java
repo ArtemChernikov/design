ba1460db1e9e41ad4987ed926aeb84b1a77f1286
Artem Chernikov <overcomingjunk@yandex.ru>
2024-04-05 13:04:21 +0300

    SimpleStack

--- src/main/java/ru/job4j/collection/SimpleStack.java
+++ src/main/java/ru/job4j/collection/SimpleStack.java
@@ -8,44 +8,13 @@ package ru.job4j.collection;
  * @version 1.1
  */
 public class SimpleStack<T> {
-    /**
-     * Поле связный список
-     */
-    private final ForwardLinked<T> linked = new ForwardLinked<>();
-    /**
-     * Поле размер стека
-     */
-    private int size = 0;
+    private ForwardLinked<T> linked = new ForwardLinked<T>();
 
-    /**
-     * Метод используется для удаления последнего элемента из стека
-     * Использует внутри {@link ForwardLinked#deleteFirst()}
-     *
-     * @return - возвращает удаленный элемент
-     */
     public T pop() {
-        T rsl = linked.deleteFirst();
-        size--;
-        return rsl;
+        return linked.deleteFirst();
     }
 
-    /**
-     * Метод используется для добавления элемента в стек
-     * Использует внутри {@link ForwardLinked#addFirst(Object)}
-     *
-     * @param value - добавляемый элемент
-     */
     public void push(T value) {
         linked.addFirst(value);
-        size++;
-    }
-
-    /**
-     * Метод используется для проверки пуст ли стек или нет
-     *
-     * @return - возвращает true - если стек пуст, false - если наоборот
-     */
-    public boolean isEmpty() {
-        return size == 0;
     }
 }
